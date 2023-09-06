c3ae9f5f054096f9447c676986ad0eff98ca127f
hexagon <hexagon@DESKTOP-1RHBAS5>
2023-09-06 15:25:27 +0900

    UserDao class, UserService class - ojs

--- ERP02/src/user/dao/UserDao.java
+++ ERP02/src/user/dao/UserDao.java
@@ -1,11 +1,13 @@
 package user.dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import jdbc.JdbcUtil;
+import jdbc.connection.ConnectionProvider;
 import user.model.User;
 
 public class UserDao {
@@ -14,7 +16,7 @@ public class UserDao {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		try {
-			pstmt = conn.prepareStatement("select * from users where userCode = ?");
+			pstmt = conn.prepareStatement("select * from users where user_code = ?");
 			pstmt.setString(1, id);
 			rs = pstmt.executeQuery();
 			User user = null;
@@ -38,11 +40,55 @@ public class UserDao {
 	}
 	
 	public void insert(Connection conn, User user) throws SQLException {
-		try(PreparedStatement pstmt = conn.prepareStatement("insert into users values(?,?,?,?,?,?,?,?,?)")) {
+		try(PreparedStatement pstmt = conn.prepareStatement("INSERT INTO users values(?,?,?,?,?,?,?,?,?)")) {
 			pstmt.setInt(1, user.getUserCode());
+			pstmt.setString(2, user.getUserPw());
+			pstmt.setString(3, user.getName());
+			pstmt.setString(4, user.getMail());
+			pstmt.setString(5, user.getRank());
+			pstmt.setString(6, user.getTel());
+			pstmt.setString(7, user.getEmpid());
+			pstmt.setInt(8, user.getEmpBr());
+			pstmt.setInt(9, user.getDeptNo());
 			
 		}
 	}
 	
-
+	public void update(Connection conn, User user) throws SQLException {
+		try(PreparedStatement pstmt = conn.prepareStatement("UPDATE users set empid = ?, user_pw = ?, name = ?, mail = ?, tel = ?")) {
+		pstmt.setString(1, user.getEmpid());
+		pstmt.setString(2, user.getUserPw());
+		pstmt.setString(3, user.getName());
+		pstmt.setString(4, user.getMail());
+		pstmt.setString(5, user.getTel());
+		pstmt.executeUpdate();
+		}
+	}
+	
+	public Boolean login(String empid, String userPw) throws SQLException{
+		boolean result = false;
+		Connection conn;
+		PreparedStatement pstmt;
+		ResultSet rs;
+		String sql = "SELECT * FROM users WHERE empid = ? AND user_pw = ?";
+		
+		try {
+			conn = ConnectionProvider.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, empid);
+			pstmt.setString(2, userPw);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				result = true;
+			}
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	
 }
--- ERP02/src/user/service/UserService.java
+++ ERP02/src/user/service/UserService.java
@@ -1,5 +1,31 @@
 package user.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import jdbc.connection.ConnectionProvider;
+import user.dao.UserDao;
+import user.model.User;
+
 public class UserService {
+
+	private UserDao userDao = new UserDao();
+
+	public User login(String empid, String userPw) {
+		try (Connection conn = ConnectionProvider.getConnection()) {
+			User user = userDao.selectById(conn, empid);
+			if (user == null) {
+				throw new RuntimeException();
+			}
+
+			if (!user.matchPassword(userPw)) {
+				throw new RuntimeException();
+			}
+
+			return new User(user.getEmpid(), user.getUserPw());
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 }
