155e8ed340cb2826ad1401b8280906610df92eed
hexagon <hexagon@DESKTOP-8HU6DB7>
2023-09-11 15:58:37 +0900

    commit - main.jsp

--- ERP02/src/notice/service/face/NoticeService.java
+++ ERP02/src/notice/service/face/NoticeService.java
@@ -1,5 +1,12 @@
 package notice.service.face;
 
+import javax.servlet.http.HttpServletRequest;
+
+import notice.model.NoticeVO;
+
 public interface NoticeService {
+
+	
+	NoticeVO getNoticeInfo(HttpServletRequest request);
 
 }
--- ERP02/src/notice/service/impl/NoticeserviceImpl.java
+++ ERP02/src/notice/service/impl/NoticeserviceImpl.java
@@ -1,7 +1,16 @@
 package notice.service.impl;
 
+import javax.servlet.http.HttpServletRequest;
+
+import notice.model.NoticeVO;
 import notice.service.face.NoticeService;
 
 public class NoticeserviceImpl implements NoticeService {
+
+	@Override
+	public NoticeVO getNoticeInfo(HttpServletRequest request) {
+		
+		return null;
+	}
 
 }
